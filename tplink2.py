ed434ddecec964eb5e65f4dde1aec1ec5fcdd1de
Humberto Jr - Macbook <halencarjunior@gmail.com>
2016-12-09 17:32:54 -0300

    Version TP-Link 2

---
+++ tplink2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import urllib.request 
+url = 'http://192.168.1.2/'
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WIN64; x64)'
+header = {'User-Agent' : user_agent}
+#req = Request(url)
+try:
+    response = urllib.request.urlopen(url)
+    print(response.read())
+except urllib.error.HTTPError as e:
+    print(e)
+except urllib.error.URLError  as u:
+    print(u)
+
+
+if ("root" in response):
+    print(url, " is vulnerable")
+else:
+    print("ok")
